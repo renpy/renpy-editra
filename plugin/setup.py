4d45adc38f9e15c89b8bedc14abeb353f8dfdb60
Tom Rothamel <tom@rothamel.us>
2012-03-19 23:29:10 -0400

    Add Json plugin.
    Json may not be part of Editra, so we ship the standard python
    version of Json as part of our plugin. (We use JSON to read in
    commands for our plugin.)

--- plugin/setup.py
+++ plugin/setup.py
@@ -7,7 +7,7 @@ setup(
       author = "Tom Rothamel",     # Your Name
       author_email = "pytom@bishoujo.us",  # Your contact
       license = "MIT",       # Plugins licensing info
-      packages = [ 'renpy_editra' ],
+      packages = [ 'renpy_editra', 'json' ],
       entry_points = '''
       [Editra.plugins]
       renpy_editra = renpy_editra:RenpyPlugin
